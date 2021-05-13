0a84933564b964574e13a61e4c1bf1b380f3aea9
AurobindoGupta <aurogpt10@gmail.com>
2021-05-14 01:43:44 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const credit_API = "https://api.themoviedb.org/3/movie/567189/credits?api_key=1c40073fce452c87ff4ca4bee5877ecf&language=en-US";
+    
+    const Img_API = "https://image.tmdb.org/t/p/w1280";
+
+
+
+    const [creditDetails, setCreditDetails] = useState([]);
+
+
+    useEffect(() => {
+       
+        getMovies(credit_API);
+        
+    }, []);
+
+    const getMovies = ( API) =>{
+    
+    
+        fetch(API)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setCreditDetails(data.results);
+      });
+    
+}
+    
+    
+    return (
+        <h3>
+             { creditDetails && creditDetails.map ((c) =>  c.name )}
+        </h3>
+    );
 }
 
 export default App;
