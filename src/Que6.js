ff696aa374217f847567b441528895860291b92e
AurobindoGupta <aurogpt10@gmail.com>
2021-06-05 21:47:07 +0530

    q6

--- src/Que6.js
+++ src/Que6.js
@@ -1,10 +1,20 @@
+
+import { useState } from "react";
+
 const Que6 = () =>{
+const[invisible,setInvisible]= useState(true);
+const buttonHandler=(e)=>{
+  console.log(e.target);
+  setInvisible(!invisible);
+}
+var x= invisible;
     return(
         
         <div>
         <hr/>
-            <div>LOADING./....</div>
-            <button>Loaded</button>
+            
+            <button onClick={buttonHandler}>{x? "Loaded":"invisible"}</button>
+            {invisible && <div>LOADING./....</div>}
         </div>
     )
 }
